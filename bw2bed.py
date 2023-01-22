63a7bd6fab30ca4e7c2c78d256cd80a265a4025a
Noam Blum <noamblum756@gmail.com>
2023-01-22 13:17:23 +0200

    Insert mean peak vvalue in placeholder column

--- bw2bed.py
+++ bw2bed.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import pyBigWig
+from numpy import mean
 
 parser = argparse.ArgumentParser("Convert BigWig file to BED file")
 parser.add_argument("input_file", help="BigWig file to convert", type=str)
@@ -19,16 +20,20 @@ with pyBigWig.open(bw_path) as bw, open(of_path, "w") as of:
         intervals = bw.intervals(chrom)
         start = None
         end = None
+        vals = []
         for interval in intervals:
             if abs(interval[2]) > threshold:
                 if start is None:
                     start = interval[0]
                     end = interval[1]
+                    vals.append(abs(interval[2]))
                 elif interval[0] == end:
                     end = interval[1]
+                    vals.append(abs(interval[2]))
                 else:
                     sign = "+" if interval[2] > 0 else "-"
-                    of.write(f"{chrom}\t{start}\t{end}\t{id}\t.\t{sign}\n")
+                    of.write(f"{chrom}\t{start}\t{end}\t{id}\t{mean(vals)}\t{sign}\n")
                     start = interval[0]
                     end = interval[1]
+                    vals = [abs(interval[2])]
                     id += 1
