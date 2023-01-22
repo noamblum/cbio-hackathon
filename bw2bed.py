9335aed34decbdd0d1ead2560f5f73f3d3536106
Noam Blum <noamblum756@gmail.com>
2023-01-22 13:23:30 +0200

    Use weighted average with subsequence length

--- bw2bed.py
+++ bw2bed.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import pyBigWig
-from numpy import mean
+from numpy import average
 
 parser = argparse.ArgumentParser("Convert BigWig file to BED file")
 parser.add_argument("input_file", help="BigWig file to convert", type=str)
@@ -21,19 +21,23 @@ with pyBigWig.open(bw_path) as bw, open(of_path, "w") as of:
         start = None
         end = None
         vals = []
+        weights = []
         for interval in intervals:
             if abs(interval[2]) > threshold:
                 if start is None:
                     start = interval[0]
                     end = interval[1]
                     vals.append(abs(interval[2]))
+                    weights.append(interval[1] - interval[0])
                 elif interval[0] == end:
                     end = interval[1]
                     vals.append(abs(interval[2]))
+                    weights.append(interval[1] - interval[0])
                 else:
                     sign = "+" if interval[2] > 0 else "-"
-                    of.write(f"{chrom}\t{start}\t{end}\t{id}\t{mean(vals)}\t{sign}\n")
+                    of.write(f"{chrom}\t{start}\t{end}\t{id}\t{average(vals, weights=weights)}\t{sign}\n")
                     start = interval[0]
                     end = interval[1]
                     vals = [abs(interval[2])]
+                    weights = [interval[1] - interval[0]]
                     id += 1
