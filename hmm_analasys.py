61e2bb5eccb0fdb5c8191c0ee94dcb661d3fe15f
Amit Yehuda <amit.yehuda@mail.huji.ac.il>
2023-01-23 16:35:55 +0200

    uploading files

--- hmm_analasys.py
+++ hmm_analasys.py
@@ -2,19 +2,17 @@ import numpy as np
 import pandas as pd
 import nanog_motif_hmm
 
-
-
 if __name__ == '__main__':
 
     motif1 = "CCATTARCAT"  # advanced known data, should be  known when running hmm
     motif2 = "CCAATCARNG"
     motif3 = "TTAATAGCCC"
-    motifs = (motif1,motif2,motif3)
-    data = {"motifs":None,"start":'',"stop":""}
-    result_sequences = [] #get results from hmm file
+    motifs = (motif1, motif2, motif3)
+    data = {"motifs": None, "start": '', "stop": ""}
+    result_sequences = []  # get results from hmm file
     motif_num = 1
 
-    for i in range(1,4):
+    for i in range(1, 4):
         with open(f"nanog_motif_predictions{i}.txt") as file:
             lines = file.readlines()
             for line in lines:
@@ -25,6 +23,5 @@ if __name__ == '__main__':
         start = [seq.rfind('B', 0, e) for seq, e in zip(result_sequences, end)]
         df = pd.DataFrame({"has_motif": has_motif, "start": start, "end": end}, index=result_sequences)
 
-
         print(df)
-        df.to_csv(f"find_motif_in_seq-motif{i}-{motifs[i-1]}.csv",seq = '\t')
+        df.to_csv(f"find_motif_in_seq-motif{i}-{motifs[i - 1]}.csv")
