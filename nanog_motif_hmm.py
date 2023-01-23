b707de36a62ce4ebca29f4a9fc133f39d1e2a705
Carmel Gadot <carmel.gadot@cs.huji.ac.il>
2023-01-23 15:58:59 +0200

    commit

--- nanog_motif_hmm.py
+++ nanog_motif_hmm.py
@@ -2,7 +2,6 @@ from itertools import groupby
 from hmmlearn import hmm
 import numpy as np
 
-
 SIZE_MOTIF = 10
 NUM_MOTIF = 1
 NUM_HIDDEN_STATES = 1 + (SIZE_MOTIF * NUM_MOTIF)
@@ -14,7 +13,8 @@ C = 1
 G = 2
 T = 3
 
-list_prediction=[]
+list_prediction = []
+
 
 # HIDDEN_STATES = [B, M1 , M2 , M3, M4, M5, M6, M7, M8, M9, M10]
 def initialize_p0_array():
@@ -25,7 +25,7 @@ def initialize_p0_array():
 
 # HIDDEN_STATES = [B, M1 , M2 , M3, M4, M5, M6, M7, M8, M9, M10]
 def initialize_emission_matrix():
-    motif = np.loadtxt("data/motifs/Nanog/homerResults/motif2.motif",
+    motif = np.loadtxt("data/motifs/Nanog/homerResults/motif1.motif",
                        comments=">")
 
     emission_matrix = np.zeros(shape=(NUM_HIDDEN_STATES, NUM_EMISSIONS))
@@ -34,17 +34,14 @@ def initialize_emission_matrix():
     for i in range(NUM_HIDDEN_STATES):
         sum_row = sum(emission_matrix[i])
         if sum_row != 1:
-            emission_matrix[i,NUM_EMISSIONS-1] += 1-sum_row
-
-
-
+            emission_matrix[i, NUM_EMISSIONS - 1] += 1 - sum_row
 
     return emission_matrix
 
 
 # HIDDEN_STATES = [B, M1 , M2 , M3, M4, M5, M6, M7, M8, M9, M10]
 def initialize_transition_matrix():
-    p = 0.5
+    p = 0.99
     transition_matrix = np.array(
         [[p, 1 - p, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -56,7 +53,7 @@ def initialize_transition_matrix():
          [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
     return transition_matrix
 
 
@@ -83,7 +80,6 @@ def fastaread(fasta_name):
         header = next(header)[1:].strip()
         seq = "".join(s.strip() for s in next(faiter))
         yield header, seq
-
 
 
 def load_and_preprocess_fasta(filename):
@@ -114,7 +110,8 @@ def train_hmm(nanog_motif_hmm, len_seqs, seqs):
         nanog_motif_hmm.transmat_[0, 1] = np.sum(
             nanog_motif_hmm.transmat_[0, 1:])
         nanog_motif_hmm.transmat_[0, 2:] = 0
-        print(nanog_motif_hmm.transmat_)
+
+
 
 def load_fasta_to_seqs_array(filename):
     seqs_array = []
@@ -130,9 +127,9 @@ def load_fasta_to_seqs_array(filename):
             elif char == "T" or char == "t":
                 seq.append([T])
 
-
         seqs_array.append(seq)
     return seqs_array
+
 
 def write_decode_to_files(nanog_motif_hmm, likelihood_output_file,
                           prediction_output_file, filename):
@@ -146,7 +143,7 @@ def write_decode_to_files(nanog_motif_hmm, likelihood_output_file,
         hidden_states_array = nanog_motif_hmm.predict(seq)
         list_seq_prediction = []
         for state in hidden_states_array:
-            if state != 0 :
+            if state != 0:
                 f_prediction_output.write('M')
                 list_seq_prediction.append('M')
             else:
@@ -158,6 +155,26 @@ def write_decode_to_files(nanog_motif_hmm, likelihood_output_file,
 
     return list_prediction
 
+
+def write_params_to_file(nanog_motif_hmm, transition_mat_output_file,
+                         p0_array_output_file):
+    f_transition_mat_output_file = open(transition_mat_output_file, 'w')
+    for array_prob in nanog_motif_hmm.transmat_:
+        for i in range(len(array_prob)):
+            f_transition_mat_output_file.write(str(array_prob[i]))
+            if i != len(array_prob) - 1:
+                f_transition_mat_output_file.write('\t')
+        f_transition_mat_output_file.write('\n')
+    f_transition_mat_output_file.close()
+
+    f_p0_array_output_file = open(p0_array_output_file, 'w')
+    for prob in nanog_motif_hmm.startprob_:
+        f_p0_array_output_file.write(str(prob))
+        f_p0_array_output_file.write('\t')
+    f_p0_array_output_file.close()
+
+
+
 def main():
     nanog_motif_hmm = initialize_nanog_motif_hmm()
 
@@ -166,11 +183,19 @@ def main():
 
     train_hmm(nanog_motif_hmm, train_len_seqs, train_seqs)
 
-    prediction_output_file = "nanog_motif_predictions.txt"
-    likelihood_output_file = "likelihood.txt"
+    prediction_output_file = "nanog_motif_predictions1.txt"
+    likelihood_output_file = "likelihood1.txt"
+    transition_mat_output_file = "transition_mat1.txt"
+    p0_array_output_file = "p0_array1.txt"
 
-    list_prediction = write_decode_to_files(nanog_motif_hmm, likelihood_output_file,
-                          prediction_output_file, ".\\data\\fasta\\Nanog.fa.txt")
+    list_prediction = write_decode_to_files(nanog_motif_hmm,
+                                            likelihood_output_file,
+                                            prediction_output_file,
+                                            ".\\data\\fasta\\Nanog.fa.txt")
+
+    write_params_to_file(nanog_motif_hmm, transition_mat_output_file,
+                         p0_array_output_file)
+
 
 if __name__ == '__main__':
     main()
