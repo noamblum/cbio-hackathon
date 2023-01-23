d1775f0ef366e03f2a619f88714e683bf953cc81
Carmel Gadot <carmel.gadot@cs.huji.ac.il>
2023-01-23 13:48:08 +0200

    commit

--- nanog_motif_hmm.py
+++ nanog_motif_hmm.py
@@ -1,15 +1,15 @@
-import argparse
+
 from itertools import groupby
 from hmmlearn import hmm
 import numpy as np
-import gzip as gz
+
 
 SIZE_MOTIF = 10
 NUM_MOTIF = 1
-NUM_HIDDEN_STATES = 1 + SIZE_MOTIF * NUM_MOTIF
+NUM_HIDDEN_STATES = 1 + (SIZE_MOTIF * NUM_MOTIF)
 NUM_EMISSIONS = 4
-MAX_NUM_ITER = 5
-NUM_TRAIN = 100
+MAX_NUM_ITER = 2
+NUM_TRAIN = 5
 A = 0
 C = 1
 G = 2
@@ -31,6 +31,14 @@ def initialize_emission_matrix():
     emission_matrix = np.zeros(shape=(NUM_HIDDEN_STATES, NUM_EMISSIONS))
     emission_matrix[0] = 1 / NUM_EMISSIONS
     emission_matrix[1:] = motif
+    for i in range(NUM_HIDDEN_STATES):
+        sum_row = sum(emission_matrix[i])
+        if sum_row != 1:
+            emission_matrix[i,NUM_EMISSIONS-1] += 1-sum_row
+
+
+
+
     return emission_matrix
 
 
@@ -38,7 +46,7 @@ def initialize_emission_matrix():
 def initialize_transition_matrix():
     p = 0.5
     transition_matrix = np.array(
-        [[p, 1 - p, 0, 0, 0, 0, 0, 0, 0, 0],
+        [[p, 1 - p, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -47,8 +55,9 @@ def initialize_transition_matrix():
          [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    return transition_matrix
 
 
 def initialize_nanog_motif_hmm():
@@ -65,17 +74,16 @@ def initialize_nanog_motif_hmm():
 
 def fastaread(fasta_name):
     """
-    Read a fasta file. For each sequence in the file, yield the header and the
-     actual sequence. You may keep this function, edit it, or delete it and
-     implement your own reader.
+    Read a fasta file. For each sequence in the file, yield the header and the actual sequence.
+    You may keep this function, edit it, or delete it and implement your own reader.
     """
-    f = gz.open(fasta_name)
-    faiter = (x[1] for x in groupby(f, lambda line:
-    line.decode().startswith(">")))
+    f = open(fasta_name)
+    faiter = (x[1] for x in groupby(f, lambda line: line.startswith(">")))
     for header in faiter:
-        header = next(header)[1:].decode().strip()
-        seq = "".join(s.decode().strip() for s in next(faiter))
+        header = next(header)[1:].strip()
+        seq = "".join(s.strip() for s in next(faiter))
         yield header, seq
+
 
 
 def load_and_preprocess_fasta(filename):
@@ -129,27 +137,38 @@ def write_decode_to_files(nanog_motif_hmm, likelihood_output_file,
     seqs_array = load_fasta_to_seqs_array(filename)
     f_likelihood_output_file = open(likelihood_output_file, 'w')
     f_prediction_output = open(prediction_output_file, 'w')
-
+    list_prediction = []
     for seq in seqs_array:
         f_likelihood_output_file.write(
             str(nanog_motif_hmm.decode(seq)[0]) + '\n')
         hidden_states_array = nanog_motif_hmm.predict(seq)
+        list_seq_prediction = []
         for state in hidden_states_array:
-            if 0 <= state <= 3:
-                f_prediction_output.write('I')
+            if state != 0 :
+                f_prediction_output.write('M')
+                list_seq_prediction.append('M')
             else:
-                f_prediction_output.write('N')
+                f_prediction_output.write('B')
+                list_seq_prediction.append('B')
+
+        list_prediction.append(list_seq_prediction)
         f_prediction_output.write('\n')
 
+    return list_prediction
 
 def main():
     nanog_motif_hmm = initialize_nanog_motif_hmm()
 
     train_seqs, train_len_seqs = \
-        load_and_preprocess_fasta("data\fasta\Nanog.fa.txt")
+        load_and_preprocess_fasta(".\\data\\fasta\\Nanog.fa.txt")
 
     train_hmm(nanog_motif_hmm, train_len_seqs, train_seqs)
 
+    prediction_output_file = "cpg_island_predictions.txt"
+    likelihood_output_file = "likelihood.txt"
+
+    list_prediction = write_decode_to_files(nanog_motif_hmm, likelihood_output_file,
+                          prediction_output_file, ".\\data\\fasta\\Nanog.fa.txt")
 
 if __name__ == '__main__':
     main()
