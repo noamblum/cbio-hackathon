f977799ec80299dfe054dfaf663b42302a5624a2
Amit Yehuda <amit.yehuda@mail.huji.ac.il>
2023-01-23 15:33:55 +0200

    creating csv files for motifs

--- hmm_analasys.py
+++ hmm_analasys.py
@@ -7,26 +7,24 @@ import nanog_motif_hmm
 if __name__ == '__main__':
 
     motif1 = "CCATTARCAT"  # advanced known data, should be  known when running hmm
+    motif2 = "CCAATCARNG"
+    motif3 = "TTAATAGCCC"
+    motifs = (motif1,motif2,motif3)
     data = {"motifs":None,"start":'',"stop":""}
-    result_sequences = nanog_motif_hmm.list_prediction #get results from hmm file
-    index = result_sequences  # seq to enter / regions
-    df = pd.DataFrame(data,index = result_sequences)
+    result_sequences = [] #get results from hmm file
     motif_num = 1
 
+    for i in range(1,4):
+        with open(f"nanog_motif_predictions{i}.txt") as file:
+            lines = file.readlines()
+            for line in lines:
+                result_sequences.append(line.strip())
+
+        has_motif = ['M' in seq for seq in result_sequences]
+        end = [seq.rfind('M') for seq in result_sequences]
+        start = [seq.rfind('B', 0, e) for seq, e in zip(result_sequences, end)]
+        df = pd.DataFrame({"has_motif": has_motif, "start": start, "end": end}, index=result_sequences)
 
 
-    for seq in result_sequences:
-        i = 0
-        while i < len(seq):
-            if (seq[i] == 'I'):
-                for j in range(6):
-                    if (seq[j] != 'I'):
-                        break
-                df[seq][motif] = True
-                df[seq]["start"] = i
-                df[seq]["end"] = i+5
-                i = i+6
-            else:
-                i = i+1
-
-    print(df)
+        print(df)
+        df.to_csv(f"find_motif_in_seq-motif{i}-{motifs[i-1]}.csv",seq = '\t')
